b54fe6a07132e7dcf2387473baed53c13004a665
Umar Hansa <umar.hansa@gmail.com>
2019-03-25 21:38:23 +0000

    Use the real GitHub API

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,7 +1,15 @@
+require('dotenv').config();
+
 const express = require('express');
 const config = require('config');
 const got = require('got');
 const escapeGoat = require('escape-goat');
+const {formatDistance} = require('date-fns');
+
+const Octokit = require('@octokit/rest')
+
+
+const {app} = require('../app-instance');
 
 const jobQueueQueries = require('../db/queries/job-queue');
 const userStarsQueries = require('../db/queries/user-stars');
@@ -9,16 +17,22 @@ const repoQueries = require('../db/queries/repo-queries');
 
 const router = express.Router();
 
-// const awairClientID = config.get('awairClientID');
+const githubToken = process.env.GITHUB_TOKEN;
+
+let octokitConfig = {};
+
+if (githubToken) {
+	octokitConfig = {
+		auth: `token ${githubToken}`
+	}
+} else {
+	console.log('No GitHub auth token found. API will be used in a non-authenticated manner');
+}
+
+const octokit = new Octokit(octokitConfig);
 
 function random(min,max) {
     return Math.floor(Math.random()*(max-min+1)+min);
-}
-
-function sleep(ms) {
-	return new Promise((resolve) => {
-		setTimeout(resolve, ms);
-	});
 }
 
 const api = {
@@ -28,147 +42,16 @@ const api = {
 		}
 
 		console.time('API Request');
-		// const response  = await octokit.activity.listReposStarredByUser({
-		// 	page,
-		// 	username: username,
-		// 	per_page: amountPerPage,
-		// 	sort: 'created',
-		// 	mediaType: {
-		// 		format: 'star+json'
-		// 	}
-		// });
-
-		await sleep(1500);
-		function getMockObj() {
-			return {
-			    "starred_at": "2011-01-16T19:06:43Z",
-			    "repo": {
-			      "id": random(1000, 9999999),
-			      "node_id": "MDEwOlJlcG9zaXRvcnkxMjk2MjY5",
-			      "name": `page-${page}-${random(1,999999)}`,
-			      "full_name": `a-${random(1,999999)}`,
-			      "owner": {
-			        "login": "octocat",
-			        "id": 1,
-			        "node_id": "MDQ6VXNlcjE=",
-			        "avatar_url": "https://github.com/images/error/octocat_happy.gif",
-			        "gravatar_id": "",
-			        "url": "https://api.github.com/users/octocat",
-			        "html_url": "https://github.com/octocat",
-			        "followers_url": "https://api.github.com/users/octocat/followers",
-			        "following_url": "https://api.github.com/users/octocat/following{/other_user}",
-			        "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
-			        "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
-			        "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
-			        "organizations_url": "https://api.github.com/users/octocat/orgs",
-			        "repos_url": "https://api.github.com/users/octocat/repos",
-			        "events_url": "https://api.github.com/users/octocat/events{/privacy}",
-			        "received_events_url": "https://api.github.com/users/octocat/received_events",
-			        "type": "User",
-			        "site_admin": false
-			      },
-			      "private": false,
-			      "html_url": "https://github.com/octocat/Hello-World",
-			      "description": "This your first repo!",
-			      "fork": false,
-			      "url": "https://api.github.com/repos/octocat/Hello-World",
-			      "archive_url": "http://api.github.com/repos/octocat/Hello-World/{archive_format}{/ref}",
-			      "assignees_url": "http://api.github.com/repos/octocat/Hello-World/assignees{/user}",
-			      "blobs_url": "http://api.github.com/repos/octocat/Hello-World/git/blobs{/sha}",
-			      "branches_url": "http://api.github.com/repos/octocat/Hello-World/branches{/branch}",
-			      "collaborators_url": "http://api.github.com/repos/octocat/Hello-World/collaborators{/collaborator}",
-			      "comments_url": "http://api.github.com/repos/octocat/Hello-World/comments{/number}",
-			      "commits_url": "http://api.github.com/repos/octocat/Hello-World/commits{/sha}",
-			      "compare_url": "http://api.github.com/repos/octocat/Hello-World/compare/{base}...{head}",
-			      "contents_url": "http://api.github.com/repos/octocat/Hello-World/contents/{+path}",
-			      "contributors_url": "http://api.github.com/repos/octocat/Hello-World/contributors",
-			      "deployments_url": "http://api.github.com/repos/octocat/Hello-World/deployments",
-			      "downloads_url": "http://api.github.com/repos/octocat/Hello-World/downloads",
-			      "events_url": "http://api.github.com/repos/octocat/Hello-World/events",
-			      "forks_url": "http://api.github.com/repos/octocat/Hello-World/forks",
-			      "git_commits_url": "http://api.github.com/repos/octocat/Hello-World/git/commits{/sha}",
-			      "git_refs_url": "http://api.github.com/repos/octocat/Hello-World/git/refs{/sha}",
-			      "git_tags_url": "http://api.github.com/repos/octocat/Hello-World/git/tags{/sha}",
-			      "git_url": "git:github.com/octocat/Hello-World.git",
-			      "issue_comment_url": "http://api.github.com/repos/octocat/Hello-World/issues/comments{/number}",
-			      "issue_events_url": "http://api.github.com/repos/octocat/Hello-World/issues/events{/number}",
-			      "issues_url": "http://api.github.com/repos/octocat/Hello-World/issues{/number}",
-			      "keys_url": "http://api.github.com/repos/octocat/Hello-World/keys{/key_id}",
-			      "labels_url": "http://api.github.com/repos/octocat/Hello-World/labels{/name}",
-			      "languages_url": "http://api.github.com/repos/octocat/Hello-World/languages",
-			      "merges_url": "http://api.github.com/repos/octocat/Hello-World/merges",
-			      "milestones_url": "http://api.github.com/repos/octocat/Hello-World/milestones{/number}",
-			      "notifications_url": "http://api.github.com/repos/octocat/Hello-World/notifications{?since,all,participating}",
-			      "pulls_url": "http://api.github.com/repos/octocat/Hello-World/pulls{/number}",
-			      "releases_url": "http://api.github.com/repos/octocat/Hello-World/releases{/id}",
-			      "ssh_url": "git@github.com:octocat/Hello-World.git",
-			      "stargazers_url": "http://api.github.com/repos/octocat/Hello-World/stargazers",
-			      "statuses_url": "http://api.github.com/repos/octocat/Hello-World/statuses/{sha}",
-			      "subscribers_url": "http://api.github.com/repos/octocat/Hello-World/subscribers",
-			      "subscription_url": "http://api.github.com/repos/octocat/Hello-World/subscription",
-			      "tags_url": "http://api.github.com/repos/octocat/Hello-World/tags",
-			      "teams_url": "http://api.github.com/repos/octocat/Hello-World/teams",
-			      "trees_url": "http://api.github.com/repos/octocat/Hello-World/git/trees{/sha}",
-			      "clone_url": "https://github.com/octocat/Hello-World.git",
-			      "mirror_url": "git:git.example.com/octocat/Hello-World",
-			      "hooks_url": "http://api.github.com/repos/octocat/Hello-World/hooks",
-			      "svn_url": "https://svn.github.com/octocat/Hello-World",
-			      "homepage": "https://github.com",
-			      "language": null,
-			      "forks_count": 9,
-			      "stargazers_count": 80,
-			      "watchers_count": 80,
-			      "size": 108,
-			      "default_branch": "master",
-			      "open_issues_count": 0,
-			      "topics": [
-			        "octocat",
-			        "atom",
-			        "electron",
-			        "API"
-			      ],
-			      "has_issues": true,
-			      "has_projects": true,
-			      "has_wiki": true,
-			      "has_pages": false,
-			      "has_downloads": true,
-			      "archived": false,
-			      "pushed_at": "2011-01-26T19:06:43Z",
-			      "created_at": "2011-01-26T19:01:12Z",
-			      "updated_at": "2011-01-26T19:14:43Z",
-			      "permissions": {
-			        "admin": false,
-			        "push": false,
-			        "pull": true
-			      },
-			      "allow_rebase_merge": true,
-			      "allow_squash_merge": true,
-			      "allow_merge_commit": true,
-			      "subscribers_count": 42,
-			      "network_count": 0
-			    }
-			   };
-			}// getMock
-
-		data = [];
-		for (let i=0; i< amountPerPage; i++) {
-			const mock = getMockObj();
-			data.push(mock)
-		}
-
-		if (random(1, 5) === 3) {
-			console.log('randomly stopping data!');
-			data = [];
-		}
-
-		let response = {
-			headers: {
-				status: '200 OK'
-			},
-			data
-		}
+		const response  = await octokit.activity.listReposStarredByUser({
+			page,
+			username,
+			per_page: amountPerPage,
+			sort: 'created',
+			mediaType: {
+				format: 'star+json'
+			}
+		});
 		console.timeEnd('API Request');
-
 		return response
 	}
 };
@@ -176,22 +59,21 @@ const api = {
 async function setupJobs() {
 	const jobProcessors = {
 		async 'fetch-stars'(username) {
-			const githubPageSize = 2;
+			const githubPageSize = 100;
 
 			console.log(`Received a fetch star job for ${username}`);
 
-			const existingStars = await userStarsQueries.getStarsForUser(username);
+			const existingStarsLength = await userStarsQueries.getStarsCountForUser(username);
 
 			let completedPages = 0;
 			let remainder = 0;
 
-			if (existingStars > 0) {
-				const existingStarsLength = existingStars;
+			if (existingStarsLength > 0) {
 				completedPages = Math.floor(existingStarsLength / githubPageSize);
 
 				remainder = existingStarsLength % githubPageSize;
 
-				console.log(`Found ${existingStarsLength} stars for ${username}. ${completedPages} (remainder ${remainder}) have already been downloaded using a page size of ${githubPageSize}.`);
+				console.log(`Found ${existingStarsLength} stars for ${username}. ${completedPages} pages (remainder ${remainder}) have already been downloaded using a page size of ${githubPageSize}.`);
 			} else {
 				console.log(`No existing stars found for ${username}`);
 			}
@@ -202,16 +84,41 @@ async function setupJobs() {
 			while (true) {
 				console.log(`Requesting page ${page}, items per page: ${githubPageSize}`);
 
-				const {data, headers} = await api.getStarsOnPage({
+				let {data, headers} = await api.getStarsOnPage({
 					amountPerPage: githubPageSize,
-					page
+					page,
+					username
 				});
+
+				function getTimeDistance(targetTime) {
+					return formatDistance(targetTime, new Date(), {
+						includeSeconds: true,
+						addSuffix: true
+					});
+				}
+
+				const rateLimit = {
+					resetTime: new Date(headers['x-ratelimit-reset'] * 1000),
+					limit: headers['x-ratelimit-limit'],
+					remaining: headers['x-ratelimit-remaining'],
+				};
+
+				rateLimit.timeDistance = getTimeDistance(rateLimit.resetTime);
+
+				app.locals.rateLimit = rateLimit;
 
 				console.log(`Page ${page}: ${headers.status}. Got ${data.length} items`, '\n');
 
 				if (!data || !data.length) {
 					console.log('No data found. Stopping.');
 					break;
+				}
+
+				if (remainder) {
+					console.log(`We found a remainder of ${remainder}. The data was previously of size: ${data.length}`);
+					data = data.slice(remainder);
+					remainder = 0;
+					console.log(`But now the data has been truncated to size ${data.length}`);
 				}
 
 				for (let item of data) {
@@ -294,7 +201,7 @@ async function setupJobs() {
 setupJobs();
 
 async function canUsernameBeSubmitted(username) {
-	if (username.length > 20 || !username.length) {
+	if (username.length > 30 || !username.length) {
 		return {
 			errorMessage: 'That username is unexpected in length'
 		};
@@ -315,7 +222,6 @@ async function canUsernameBeSubmitted(username) {
 	if (mostRecent) {
 		const mostRecentCreatedTime = new Date(mostRecent.database_entry_updated_at);
 		const howLongAgo = Date.now() - mostRecentCreatedTime;
-		console.info(`Existing thing found, was created ${howLongAgo}`);
 
 		const tenMinutes = ((1000 * 60) * 10);
 
@@ -370,6 +276,28 @@ router.post('/submit-github-username', async (req, res) => {
 	return res.redirect('/');
 });
 
+router.get('/user/:rawUsername', async (req, res) => {
+	const username = escapeGoat.escape(req.params.rawUsername);
+	const stars = await userStarsQueries.getStarsForUser(username);
+
+	if (!stars.length) {
+		req.flash('messages', {
+			status: 'danger',
+			value: `Couldn't find ${username}`
+		});
+
+		return res.redirect('/');
+	}
+
+	const renderObject = {
+		messages: req.flash('messages'),
+		stars,
+		username
+	};
+
+	res.render('user', renderObject);
+});
+
 router.get('/jobs', async (req, res) => {
 	const jobs = await jobQueueQueries.getAllJobs();
 
@@ -382,8 +310,21 @@ router.get('/jobs', async (req, res) => {
 });
 
 router.get('/', async (req, res) => {
+	const distinctUsers = await userStarsQueries.getDistinctUsersWithStarCount();
+
 	const renderObject = {
-		messages: req.flash('messages')
+		messages: req.flash('messages'),
+		distinctUsers: distinctUsers.sort((itemA, itemB) => {
+			const countA = itemA.count;
+			const countB = itemB.count;
+
+			if (countA < countB) {
+			  return -1;
+			}
+			if (countA > countB) {
+			  return 1;
+			}
+		}).reverse()
 	};
 
 	res.render('index', renderObject);
